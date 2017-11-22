474eb059f96ba1158081000d0998d741e6d47abc
遼太郎 金 <150568@jc-21.jp>
2017-11-22 16:14:11 +0900

    カメラ

--- SamplePage/SamplePage/CameraPage.xaml.cs
+++ SamplePage/SamplePage/CameraPage.xaml.cs
@@ -1,20 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+
+using ZXing.Mobile;
+using ZXing.Net.Mobile.Forms;
 
 namespace SamplePage
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+    
     public partial class CameraPage : ContentPage
     {
         public CameraPage()
         {
             InitializeComponent();
+        }
+
+        async void ScanButtonClicked(object sender, EventArgs s)
+        {
+            var scanPage = new ZXingScannerPage()
+            {
+                DefaultOverlayTopText = "バーコードを読み取ります",
+                DefaultOverlayBottomText = "",
+            };
+
+            // スキャナページを表示
+            await Navigation.PushAsync(scanPage);
+
+            scanPage.OnScanResult += (result) =>
+            {
+                // スキャン停止
+                scanPage.IsScanning = false;
+
+                // PopAsyncで元のページに戻り、結果をダイアログで表示
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                    await DisplayAlert("スキャン完了", result.Text, "OK");
+                });
+
+                //scanedData.Add(result.Text);
+            };
         }
     }
 }
