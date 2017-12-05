840b0800e17503e6d0fbec54a0b3a74542365918
遼太郎 金 <150568@jc-21.jp>
2017-12-05 14:54:03 +0900

    noob

--- SamplePage/SamplePage/App.xaml.cs
+++ SamplePage/SamplePage/App.xaml.cs
@@ -9,6 +9,49 @@ namespace SamplePage
 {
     public partial class App : Application
     {
+        //データベースのパスを格納
+        public static string dbPath;
+
+
+        public App(string dbPath)
+        {
+            //AppのdbPathに引数のパスを設定します
+            App.dbPath = dbPath;
+            
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
+
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SamplePage
+{
+    public partial class App : Application
+    {
         public App(string dbPath)
         {
             //AppのdbPathに引数のパスを設定します
@@ -34,4 +77,4 @@ namespace SamplePage
             // Handle when your app resumes
         }
     }
-}
+}*/
